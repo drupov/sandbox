5dd4dba9b4465ed685cd95652dbaa3c72319348c
drupov <dimitar@precessionmedia.com>
2015-02-17 13:50:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<title>Sandbox</title>
-    		<meta charset="UTF-8">
+		<meta charset="UTF-8">
 		<link rel="stylesheet" href="css/style.css" type="text/css">		
 		<script src="js/jquery-1.8.2.min.js" type="text/javascript"></script>		
 		<script src="js/scripts.js" type="text/javascript"></script>
